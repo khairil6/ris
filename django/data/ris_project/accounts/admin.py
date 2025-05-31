3ee7973593d62330d198c1c4b94ebb1dbacf1d9f
khairil <khairil6@yahoo.com>
2025-05-31 13:00:00 +0800

    added model admin

--- django/data/ris_project/accounts/admin.py
+++ django/data/ris_project/accounts/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin
+# data/accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Institution, User
+
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ("name", "hl7_facility_code", "dicom_station_name")
+    search_fields = ("name", "hl7_facility_code")
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # Show the usual User fields, plus 'institution', 'role', 'dicom_ae_title'
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Institution & Role", {"fields": ("institution", "role", "dicom_ae_title")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "institution", "role", "dicom_ae_title", "password1", "password2"),
+        }),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "institution", "role", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "institution", "role")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
--- django/data/ris_project/ris_app/admin.py
+++ django/data/ris_project/ris_app/admin.py
@@ -1,3 +1,49 @@
-from django.contrib import admin
+# data/ris_app/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Patient, Order, Study, Series, Instance, Report, AuditLog
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("patient_id", "name", "dob")
+    search_fields = ("patient_id", "name")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("accession", "procedure_code", "status")
+    list_filter = ("status",)
+    search_fields = ("accession", "procedure_code")
+
+
+@admin.register(Study)
+class StudyAdmin(admin.ModelAdmin):
+    list_display = ("study_instance_uid", "patient")
+    search_fields = ("study_instance_uid", "patient__patient_id", "patient__name")
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ("series_instance_uid", "study")
+    search_fields = ("series_instance_uid", "study__study_instance_uid")
+
+
+@admin.register(Instance)
+class InstanceAdmin(admin.ModelAdmin):
+    list_display = ("sop_instance_uid", "series")
+    search_fields = ("sop_instance_uid", "series__series_instance_uid")
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ("study", "status")
+    search_fields = ("study__study_instance_uid", "status")
+
+
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ("timestamp", "user", "action", "study")
+    list_filter = ("action", "user")
+    search_fields = ("user__username", "action", "study__study_instance_uid")
+    date_hierarchy = "timestamp"
